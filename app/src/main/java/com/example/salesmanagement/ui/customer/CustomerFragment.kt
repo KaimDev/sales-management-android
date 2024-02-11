2830081bbd128488fbe5fb74d5d110f9a7f82afd
Johan Paiz <johanedfb@gmail.com>
2024-02-11 00:49:44 -0600

    refactor: Move Customer List to CustomerViewModel

--- app/src/main/java/com/example/salesmanagement/ui/customer/CustomerFragment.kt
+++ app/src/main/java/com/example/salesmanagement/ui/customer/CustomerFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,14 +12,6 @@ import com.example.salesmanagement.models.Customer
 
 class CustomerFragment : Fragment()
 {
-    val Customers = listOf(
-        Customer(1, "John Doe", "1234567890", "john@email.com", "123 Main St"),
-        Customer(2, "Jane Doe", "0987654321", "jane@email.com","456 Main St"),
-        Customer(3, "John Smith", "1234567890", "", "123 Main St"),
-        Customer(4, "Lourdes Lopez", "1234567890", "lopez@email.com", ""),
-        Customer(5, "John Doe", "1234567890", null, null)
-    )
-
     private var _binding: FragmentCustomerBinding? = null
 
     private val binding get() = _binding!!
@@ -36,19 +27,15 @@ class CustomerFragment : Fragment()
         _binding = FragmentCustomerBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        binding.fabAddCustomer.setOnClickListener {
-            customerViewModel.GoToAddCustomer()
-        }
-
-        initRecyclerView()
+        initRecyclerView(customerViewModel.customers.value!!)
 
         return root
     }
 
-    private fun initRecyclerView()
+    private fun initRecyclerView(customers: List<Customer>)
     {
         val rcCustomer = binding.rvCustomer
         rcCustomer.layoutManager = LinearLayoutManager(this.context)
-        rcCustomer.adapter = CustomerAdapter(Customers)
+        rcCustomer.adapter = CustomerAdapter(customers)
     }
 }
--- app/src/main/java/com/example/salesmanagement/ui/customer/CustomerViewModel.kt
+++ app/src/main/java/com/example/salesmanagement/ui/customer/CustomerViewModel.kt
@@ -3,11 +3,20 @@ package com.example.salesmanagement.ui.customer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.salesmanagement.models.Customer
 
 class CustomerViewModel : ViewModel()
 {
-    fun GoToAddCustomer()
-    {
-
+    // List of customers
+    private val _customers = MutableLiveData<List<Customer>>().apply {
+        value = listOf(
+            Customer(1, "John Doe", "1234567890", "john@email.com", "123 Main St"),
+            Customer(2, "Jane Doe", "0987654321", "jane@email.com", "456 Main St"),
+            Customer(3, "John Smith", "1234567890", "", "123 Main St"),
+            Customer(4, "Lourdes Lopez", "1234567890", "lopez@email.com", ""),
+            Customer(5, "John Doe", "1234567890", null, null)
+        )
     }
+
+    val customers: LiveData<List<Customer>> = _customers
 }
