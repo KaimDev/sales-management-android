6546ae68d6039408d70e2131f591dc3a0db22b09
Johan Paiz <johanedfb@gmail.com>
2024-02-24 22:55:05 -0600

    feat: Delete Customer Model, Customer Entity now is being used

--- app/src/main/java/com/example/salesmanagement/models/Customer.kt
+++
@@ -1,9 +0,0 @@
-package com.example.salesmanagement.models
-
-data class Customer (
-    val id: Int,
-    val name: String,
-    val phone: String,
-    val email: String?,
-    val address: String?
-)
