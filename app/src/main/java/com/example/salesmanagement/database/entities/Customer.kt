44ee0fdccd7f3740f24626b6e846ce9b18f68c68
Johan Paiz <johanedfb@gmail.com>
2024-02-24 21:59:32 -0600

    feat: Set Email and Address in Customer Entity as Nullable

--- app/src/main/java/com/example/salesmanagement/database/entities/Customer.kt
+++ app/src/main/java/com/example/salesmanagement/database/entities/Customer.kt
@@ -9,6 +9,6 @@ class Customer(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "phone") val phone: String,
-    @ColumnInfo(name = "email") val email: String,
-    @ColumnInfo(name = "address") val address: String
+    @ColumnInfo(name = "email") val email: String?,
+    @ColumnInfo(name = "address") val address: String?
 )
